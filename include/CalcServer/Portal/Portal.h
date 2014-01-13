d0c303f69768a2f068dc0d34f5a0dc79d4e39510
Cercos-Pita J.L <jlcercos@gmail.com>
2014-01-13 13:42:22 +0100

    Fixed bad licensed file

--- include/CalcServer/Portal/Portal.h
+++ include/CalcServer/Portal/Portal.h
@@ -1,28 +1,20 @@
 /*
- * This source file is part of AQUA-gpusph.
+ *  This file is part of AQUAgpusph, a free CFD program based on SPH.
+ *  Copyright (C) 2012  Jose Luis Cercos Pita <jl.cercos@upm.es>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ *  AQUAgpusph is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Library General Public License for more details.
+ *  AQUAgpusph is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ *  You should have received a copy of the GNU General Public License
+ *  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.
  */
-/*
-	Authors:
-	- Cercos Pita, Jose Luis
-	- Miguel Gonzalez, Leo
-	- Rey Villaverde, Anton
-	- Saelices, Jaime
-	- Souto Iglesias, Antonio
-*/
 
 #ifndef PORTAL_H_INCLUDED
 #define PORTAL_H_INCLUDED
