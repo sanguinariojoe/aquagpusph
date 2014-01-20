85e16b69a81439d7b0c22ee5ded72c151aca66a3
Cercos-Pita J.L <jlcercos@gmail.com>
2014-01-20 14:16:36 +0100

    Updated the lines count script for the git folder

--- countLines.sh
+++ countLines.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Exclude dat/ods/pdf resource files, tex documentation files, and
+# Exclude ods/pdf resource files, tex documentation files, and
 # txt files that are not CMake related, svg and png images.
-wc -l `find ./ ! \( -name "*.dat" -o -name "*.ods" -o -name "*.pdf" -o -name "*.tex" -o -name "*.bib" -o \( -name "*.txt" -a ! -name "CMakeLists.txt" \) -o -name "*.png" -o -name "*.svg" \)` | sort -n
+wc -l `find ./ ! \( -wholename "*.git*" -o -name "*.ods" -o -name "*.pdf" -o -name "*.tex" -o -name "*.bib" -o \( -name "*.txt" -a ! -name "CMakeLists.txt" \) -o -name "*.png" -o -name "*.svg" \)` | sort -n
 
