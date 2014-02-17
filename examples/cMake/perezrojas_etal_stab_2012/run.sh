e7ffbdde0cd36958e27f22c4679bbdd0e0845c2e
Cercos-Pita J.L <jlcercos@gmail.com>
2014-02-17 10:28:48 +0100

    Moved to a Python based ploting method

--- examples/cMake/perezrojas_etal_stab_2012/run.sh
+++ examples/cMake/perezrojas_etal_stab_2012/run.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 if [[ $1 == "--run" ]]; then
-	@BINARY_DIR@/AQUAgpusph -i @EXAMPLE_DEST_DIR@/Main.xml --no-reassembly
+	rm -f Sensors.dat
+    rm -f AQUAgpusph.save.*.xml
+    rm -f energy.*.dat
+    rm -f log.*.html
+    rm -f output.*.vtu
+    rm -f output.pvd
+	@BINARY_DIR@/AQUAgpusph -i @EXAMPLE_DEST_DIR@/Main.xml
 elif [[ $1 == "--plot" ]]; then
-	if [ ! -f zMovement.dat ]; then
+	if [ ! -f Motion.dat ]; then
 		echo ""
-		echo "zMovement.dat output file is not yet ready!"
+		echo "Failure while opening Motion.dat output file"
 		echo ""
-		echo "    You must execute run.sh --plot on the same folder where run.sh --run has been launched"
+		echo " * You must execute run.sh --plot on the same folder where run.sh --run has been launched"
+		echo " * AQUAgpusph may take some time before generate this file"
 		echo ""
 		exit 255
 	fi
-	gnuplot @EXAMPLE_DEST_DIR@/doc/plot.gnuplot
+	python @EXAMPLE_DEST_DIR@/doc/plot.py
 else
 	echo ""
 	echo "Usage: run.sh [--run/--plot]"
 	echo ""
 	echo "run.sh --run"
-	echo "    Runs the simulation. You can stop simulation pressing 'c' key"
+	echo "    Runs the simulation. You can stop simulation pressing the 'c' key"
 	echo "run.sh --plot"
 	echo "    Plots in real time the output results"
 	echo ""
