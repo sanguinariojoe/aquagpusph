1e5b7c06fe399435b1cf0f1e22bc0f346aac50d3
Cercos-Pita J.L <jlcercos@gmail.com>
2014-02-17 10:03:42 +0100

    Updated the case generation

--- examples/perezrojas_etal_stab_2012/Create.py
+++ examples/perezrojas_etal_stab_2012/Create.py
@@ -1,16 +1,16 @@
 #! /usr/bin/env python
-#########################################################
-#                                                       #
-#    #    ##   #  #   #                           #     #
-#   # #  #  #  #  #  # #                          #     #
-#  ##### #  #  #  # #####  ##  ###  #  #  ## ###  ###   #
-#  #   # #  #  #  # #   # #  # #  # #  # #   #  # #  #  #
-#  #   # #  #  #  # #   # #  # #  # #  #   # #  # #  #  #
-#  #   #  ## #  ##  #   #  ### ###   ### ##  ###  #  #  #
-#                            # #             #          #
-#                          ##  #             #          #
-#                                                       #
-#########################################################
+#########################################################################
+#                                                                       #
+#            #    ##   #  #   #                           #             #
+#           # #  #  #  #  #  # #                          #             #
+#          ##### #  #  #  # #####  ##  ###  #  #  ## ###  ###           #
+#          #   # #  #  #  # #   # #  # #  # #  # #   #  # #  #          #
+#          #   # #  #  #  # #   # #  # #  # #  #   # #  # #  #          #
+#          #   #  ## #  ##  #   #  ### ###   ### ##  ###  #  #          #
+#                                    # #             #                  #
+#                                  ##  #             #                  #
+#                                                                       #
+#########################################################################
 #
 #  This file is part of AQUA-gpusph, a free CFD program based on SPH.
 #  Copyright (C) 2012  Jose Luis Cercos Pita <jl.cercos@upm.es>
@@ -28,27 +28,18 @@
 #  You should have received a copy of the GNU General Public License
 #  along with AQUA-gpusph.  If not, see <http://www.gnu.org/licenses/>.
 #
-#########################################################
-#
-# Create.py
-#
-# Script from example LateralWater_1x_Violeau. This script
-# can create the fluid and solid file for this case, in dat format.
-#
-# Input data:
-#
-#     h  = Fluid height
-#     H  = Tank height
-#     L  = Tank length
-#     D  = Tank width
-#     n  = Stimated number of particles
-#     cs = Sound speed
-#     refd  = Density reference
-#     gamma = Exponent for Batchelor'67 state equation
-#
-#########################################################
+#########################################################################
 
-# General settings
+# Generate the fluid particles and the boundary elements for the example
+# perezrojas_etal_stab_2012, Correponding to the tuned liquid damper SPHERIC
+# test case number 9. See
+# doc/SOUTOIGLESIAS_BOTIA_SPHERIC_TESTCASE9_TLD.pdf
+# In this example the water case is considered.
+
+
+# Input data
+# ==========
+
 cs    = 15.0
 refd  = 998.0
 gamma = 7.0
@@ -61,34 +52,41 @@ D  = 0.062
 h  = 0.092
 # Stimated required number of fluid particles
 n  = 250000
-# Calculate the number of particles at X/Y/Z, and the distances associated
-nx     = int(round( (n*D*D / h / L)**(1.0/3.0) ))
-ny     = int(round( (n*L*L / h / D)**(1.0/3.0) ))
-nz     = n / (nx*ny)
-n      = nx*ny*nz
-# Calculate distance between particles
-drx    = D/nx
-dry    = L/ny
-drz    = h/nz
-dr     = max(drx,dry,drz)
+
+# Dimensions and number of particles readjustment
+# ===============================================
+
+nx = int(round((n * D * D / h / L)**(1.0 / 3.0)))
+ny = int(round((n * L * L / h / D)**(1.0 / 3.0)))
+nz = n // (nx * ny)
+n = nx * ny * nz
+
+drx = D/nx
+dry = L/ny
+drz = h/nz
+dr = max(drx, dry, drz)
 hFluid = nz*drz
-# Calculate solid particles
-Nx     = nx
-Ny     = ny
-Nz     = int(round( H/drz ))+1
+
+# Solid boundary elements
+Nx = nx
+Ny = ny
+Nz = int(round(H / drz)) + 1
+
 # Correct tank dimensions
-L      = Ny*dr
-D      = Nx*dr
-H      = Nz*dr
-# Correct DeLeffe vertices distance
+L = Ny * dr
+D = Nx * dr
+H = Nz * dr
+
 DeLeffeDistFactor = 1
-Nx     = DeLeffeDistFactor*Nx
-Ny     = DeLeffeDistFactor*Ny
-Nz     = DeLeffeDistFactor*Nz
-N      = 2*Nx*Ny + 2*Nx*Nz + 2*Ny*Nz
-# Fluid properties
-prb    = cs*cs*refd/gamma
-# Open output file, and write initial data
+Nx = DeLeffeDistFactor * Nx
+Ny = DeLeffeDistFactor * Ny
+Nz = DeLeffeDistFactor * Nz
+N = 2 * Nx * Ny + 2 * Nx * Nz + 2 * Ny * Nz
+
+# Fluid generation
+# ================
+prb = cs * cs * refd / gamma
+
 print("Opening output file...")
 output = open("Fluid.dat","w")
 string = """#############################################################
@@ -126,107 +124,132 @@ string = """#############################################################
 """
 output.write(string)
 print(string)
-# Write data
+
 string = """
     Writing fluid particles...
 """
 print(string)
+
 Percentage = -1
-for i in range(0,n):
-	if Percentage != (i*100) / n:
-		Percentage = (i*100) / n
-		if not Percentage%10:
-			string = '    %d%%' % (Percentage)
-			print(string)
-	j      = i
-	idd    = j/ny
-	idx    = idd / nz
-	idy    = j%ny
-	idz    = idd % nz
-	imove  = 1
-	pos    = (idx*dr - 0.5*(D-dr), idy*dr - 0.5*(L-dr), idz*dr + 0.5*dr)
-	press  = refd*g*(hFluid - pos[2]);
-	dens   = pow( press/prb + 1.0, 1.0/gamma )*refd;
-	mass   = dens*dr**3.0
-	string = "%g %g %g 0.0 0.0 0.0 0.0 0.0 0.0 %g %d %g %g\n" % (pos[0], pos[1], pos[2], mass, imove, dens, cs)
-	output.write(string)
-print('    100%%')
-# Write data
+for i in range(0, n):
+    if Percentage != (i * 100) / n:
+        Percentage = (i * 100) / n
+        if not Percentage % 10:
+            string = '    {}%'.format(Percentage)
+            print(string)
+    j = i
+    idd = j / ny
+    idx = idd / nz
+    idy = j % ny
+    idz = idd % nz
+    imove = 1
+    pos = (idx * dr - 0.5 * (D - dr),
+           idy * dr - 0.5 * (L - dr),
+           idz * dr + 0.5 * dr)
+    press = refd * g * (hFluid - pos[2]);
+    dens = pow(press / prb + 1.0, 1.0 / gamma) * refd;
+    mass = dens * dr**3.0
+    string = "{} {} {},{} {} {},{} {} {},{} {} {},{},{},{},{}\n".format(
+        pos[0], pos[1], pos[2],
+        0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0,
+        dens,
+        0.0,
+        mass,
+        imove)
+    output.write(string)
+print('    100%')
+
 string = """
     Writing nodes...
 """
 print(string)
-Percentage=-1
-for i in range(0,N):
-	if Percentage != (i*100) / N:
-		Percentage = (i*100) / N
-		if not Percentage%10:
-			string = '    %d%%' % (Percentage)
-			print(string)
-	if(i < Nx*Ny):                             # Bottom
-		j      = i
-		idd    = j
-		idz    = 0
-		idx    = idd / Ny + 0.5
-		idy    = idd % Ny + 0.5
-		normal = [0.0,0.0,-1.0]
-	elif(i < 2*Nx*Ny):                         # Roof
-		j      = i-Nx*Ny
-		idd    = j
-		idz    = Nz
-		idx    = idd / Ny + 0.5
-		idy    = idd % Ny + 0.5
-		normal = [0.0,0.0,1.0]
-	elif(i < 2*Nx*Ny + Nx*Nz):                 # Left
-		j      = i-2*Nx*Ny
-		idd    = j
-		idy    = 0
-		idx    = idd / Nz + 0.5
-		idz    = idd % Nz + 0.5
-		normal = [0.0,-1.0,0.0]
-	elif(i < 2*Nx*Ny + 2*Nx*Nz):               # Right
-		j      = i - 2*Nx*Ny - Nx*Nz
-		idd    = j
-		idy    = Ny
-		idx    = idd / Nz + 0.5
-		idz    = idd % Nz + 0.5
-		normal = [0.0,1.0,0.0]
-	elif(i < 2*Nx*Ny + 2*Nx*Nz + Ny*Nz):       # Front
-		j      = i - 2*Nx*Ny - 2*Nx*Nz
-		idd    = j
-		idx    = 0
-		idy    = idd / Nz + 0.5
-		idz    = idd % Nz + 0.5
-		normal = [-1.0,0.0,0.0]
-	elif(i < 2*Nx*Ny + 2*Nx*Nz + 2*Ny*Nz):     # Back
-		j      = i - 2*Nx*Ny - 2*Nx*Nz - Ny*Nz
-		idd    = j
-		idx    = nx
-		idy    = idd / Nz + 0.5
-		idz    = idd % Nz + 0.5
-		normal = [1.0,0.0,0.0]
-	pos = (idx*dr/DeLeffeDistFactor - 0.5*D, 
-	       idy*dr/DeLeffeDistFactor - 0.5*L, 
-	       idz*dr/DeLeffeDistFactor)
-	if pos[2] <= hFluid:
-		press = refd*g*(hFluid - pos[2]);
-		dens  = pow( press/prb + 1.0, 1.0/gamma )*refd;
-	else:
-		dens  = refd;
-		press = prb*pow( dens/refd , gamma - 1.0 );
-	imove = -1
-	mass  = (dr/DeLeffeDistFactor)**2.0
-	string = "%g %g %g %g %g %g 0.0 0.0 0.0 %g %d %g %g\n" % (pos[0], pos[1], pos[2], normal[0], normal[1], normal[2], mass, imove, dens, cs)
-	output.write(string)
-print('    100%%')
+Percentage = -1
+for i in range(0, N):
+    if Percentage != (i * 100) / N:
+        Percentage = (i * 100) / N
+        if not Percentage % 10:
+            string = '    {}%'.format(Percentage)
+            print(string)
+    # Bottom
+    if(i < Nx * Ny):
+        j = i
+        idd = j
+        idz = 0
+        idx = idd / Ny + 0.5
+        idy = idd % Ny + 0.5
+        normal = [0.0, 0.0, -1.0]
+    # Roof
+    elif(i < 2 * Nx * Ny):
+        j = i - Nx * Ny
+        idd = j
+        idz = Nz
+        idx = idd / Ny + 0.5
+        idy = idd % Ny + 0.5
+        normal = [0.0, 0.0, 1.0]
+    # Left
+    elif(i < 2 * Nx * Ny + Nx * Nz):
+        j = i - 2 * Nx * Ny
+        idd = j
+        idy = 0
+        idx = idd / Nz + 0.5
+        idz = idd % Nz + 0.5
+        normal = [0.0, -1.0, 0.0]
+    # Right
+    elif(i < 2 * (Nx * Ny + Nx * Nz)):
+        j = i - 2 * Nx * Ny - Nx * Nz
+        idd = j
+        idy = Ny
+        idx = idd / Nz + 0.5
+        idz = idd % Nz + 0.5
+        normal = [0.0, 1.0, 0.0]
+    # Front
+    elif(i < 2 * (Nx * Ny + Nx * Nz) + Ny * Nz):
+        j = i - 2 * (Nx * Ny + Nx * Nz)
+        idd = j
+        idx = 0
+        idy = idd / Nz + 0.5
+        idz = idd % Nz + 0.5
+        normal = [-1.0, 0.0, 0.0]
+    # Back
+    elif(i < 2 * (Nx * Ny + Nx * Nz + Ny * Nz)):
+        j = i - 2 * (Nx*Ny + Nx * Nz) - Ny * Nz
+        idd = j
+        idx = nx
+        idy = idd / Nz + 0.5
+        idz = idd % Nz + 0.5
+        normal = [1.0, 0.0, 0.0]
+    pos = (idx * dr / DeLeffeDistFactor - 0.5 * D, 
+           idy * dr / DeLeffeDistFactor - 0.5 * L, 
+           idz * dr / DeLeffeDistFactor)
+    if pos[2] <= hFluid:
+        press = refd * g * (hFluid - pos[2]);
+        dens  = pow(press / prb + 1.0, 1.0 / gamma) * refd;
+    else:
+        dens  = refd;
+        press = prb*pow(dens / refd, gamma - 1.0 );
+    imove = -1
+    mass  = (dr / DeLeffeDistFactor)**2.0
+    string = "{} {} {},{} {} {},{} {} {},{} {} {},{},{},{},{}\n".format(
+        pos[0], pos[1], pos[2],
+        normal[0], normal[1], normal[2],
+        0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0,
+        dens,
+        0.0,
+        mass,
+        imove)
+    output.write(string)
+print('    100%')
 string = """
-%d particles has been written into file (%d of fluid).
+{} particles has been written into file ({} of fluid).
 PLEASE, CHANGE THIS VALUE IN THE FLUID XML DEFINITION.
-""" % (n+N, n)
+""".format(n + N, n)
 print(string)
 # Print some useful data
-print('Particle distance:  %g' % (dr))
+print('Particle distance:  {}'.format(dr))
 print('PLEASE, CHANGE THIS VALUE IN THE SPH XML DEFINITION.')
-print('Fluid height error: %g' % (abs(h - hFluid)))
+print('Fluid height error: {}'.format(abs(h - hFluid)))
 # Clean up
 output.close()
