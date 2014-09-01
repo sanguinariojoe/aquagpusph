ae90c13bc650c1b2b2449df8685485cbf0a8b5bc
Jose Luis Cercos-Pita <jl.cercos@upm.es>
2014-09-01 14:35:13 +0200

    Added the file definition

--- src/FileManager.cpp
+++ src/FileManager.cpp
@@ -16,6 +16,11 @@
  *  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/** @file
+ * @brief Input and output files managing.
+ * (See Aqua::InputOutput::FileManager for details)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
