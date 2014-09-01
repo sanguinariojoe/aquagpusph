1dcc25f072c7c1f8a1e15581278687c09dd76279
Jose Luis Cercos-Pita <jl.cercos@upm.es>
2014-09-01 13:19:30 +0200

    Fixed briefs and references

--- include/AuxiliarMethods.h
+++ include/AuxiliarMethods.h
@@ -28,12 +28,14 @@
 namespace Aqua{
 
 /** Returns if a key press event has been registered.
+ *
  * @return 0 if no keys have been pressed, 1 otherwise.
  */
 int isKeyPressed();
 
 /** Compute a value which, being power of two, is greater or equal than
  * @paramname{x}.
+ *
  * @param x Number to round up to a power of two.
  * @return Next value which is power of two (it can be the same input value
  * @paramname{x}).
@@ -41,6 +43,7 @@ int isKeyPressed();
 unsigned int nextPowerOf2(unsigned int x);
 
 /** Compute if a value is power of 2.
+ *
  * @param x Value to test.
  * @return 1 if it is a power of two, 0 otherwise.
  */
@@ -48,6 +51,7 @@ unsigned int isPowerOf2(unsigned int x);
 
 /** Compute a value, which being divisible by @paramname{divisor}, is greater or equal
  * than @paramname{x}.
+ *
  * @param x Number to round up.
  * @param divisor Divisor.
  * @return Rounded up number.
@@ -55,6 +59,7 @@ unsigned int isPowerOf2(unsigned int x);
 unsigned int roundUp(unsigned int x, unsigned int divisor);
 
 /** Load an OpenCL kernel from a file.
+ *
  * @param kernel The output kernel identifier.
  * @param program The output program identifier.
  * @param context Context from where the program should be loaded.
@@ -82,28 +87,32 @@ unsigned int roundUp(unsigned int x, unsigned int divisor);
  */
 size_t loadKernelFromFile(cl_kernel* kernel, cl_program* program,
                           cl_context context, cl_device_id device,
-	                      const char* path, const char* entry_point,
-	                      const char* flags, const char* header=NULL);
+                          const char* path, const char* entry_point,
+                          const char* flags, const char* header=NULL);
 
 /** Gets the folder path which contains the file @paramname{file_path}.
+ *
  * @param file_path The file path.
  * @return The folder.
  */
 const char* getFolderFromFilePath(const char* file_path);
 
 /** Gets the file extension.
+ *
  * @param file_path The file path.
  * @return Extension of the file.
  */
 const char* getExtensionFromFilePath(const char* file_path);
 
-/** Method that returns if the file @paramname{file_path} exist on the system
+/** Method that returns if the file @paramname{file_path} exist on the system.
+ *
  * @param file_name The file path.
  * @return 0 if the file can not be found in the system, 1 otherwise.
  */
 int isFile(const char* file_name);
 
 /** Load a file returning it as a characters array.
+ *
  * @param source_code Readed file content.
  * @param file_name The file path.
  * @return Length of the source code array.
@@ -115,6 +124,7 @@ int isFile(const char* file_name);
 size_t readFile(char* source_code, const char* file_name);
 
 /** Send an argument to an OpenCL kernel.
+ *
  * @param kernel Kernel that must receive the argument.
  * @param index Index of the argument into the kernel @paramname{kernel}.
  * @param size Memory size of the argument.
@@ -124,20 +134,25 @@ size_t readFile(char* source_code, const char* file_name);
 int sendArgument(cl_kernel kernel, int index, size_t size, void* ptr);
 
 /** Compute the maximum local work size allowed by the device.
+ *
  * @param n Amount of data to operate in kernel (aiming threads to launch).
  * @param queue Command queue.
  * @return The local work size, 0 if it is not possible to find a valid value.
  */
 size_t getLocalWorkSize(cl_uint n, cl_command_queue queue);
 
-/** Compute the global work size needed.
+/** Compute the global work size needed to compute @paramname{n} threads.
+ *
  * @param n Amount of data to operate in kernel (aiming threads to launch).
  * @param local_work_size The local work size which will be applied.
  * @return The required global work size.
+ *
+ * @see roundUp()
  */
 size_t getGlobalWorkSize(cl_uint n, size_t local_work_size);
 
 /** Gets the minimum of two values.
+ *
  * @param a First value.
  * @param b Second value.
  * @return Minimum value.
@@ -145,6 +160,7 @@ size_t getGlobalWorkSize(cl_uint n, size_t local_work_size);
 template <typename T> inline T min(T a, T b){return (a>b)?b:a;}
 
 /** Gets the maximum of two values.
+ *
  * @param a First value.
  * @param b Second value.
  * @return Maximum value.
@@ -152,6 +168,7 @@ template <typename T> inline T min(T a, T b){return (a>b)?b:a;}
 template <typename T> inline T max(T a, T b){return (a<b)?b:a;}
 
 /** Clamps a value between the bounds.
+ *
  * @param x Value to adjust into the bounds.
  * @param a Minimum value.
  * @param b Maximum value.
@@ -160,35 +177,42 @@ template <typename T> inline T max(T a, T b){return (a<b)?b:a;}
 inline float clamp(float x, float a, float b){return x < a ? a : (x > b ? b : x);}
 
 /** Return a null vector.
+ *
  * @return zeroes vector.
  */
 vec Vzero();
 
 /** Return the x direction unit vector.
+ *
  * @return x direction unit vector.
  */
 vec Vx();
 
 /** Return the y direction unit vector.
+ *
  * @return y direction unit vector.
  */
 vec Vy();
 
 #ifdef HAVE_3D
 /** Return the z direction unit vector.
+ *
+ * @remarks Only available in the 3D version.
  * @return z direction unit vector.
  */
 vec Vz();
 #endif
 
 /** Multiply a vector by a scalar.
+ *
  * @param n Scalar to operate.
  * @param v Vector to operate.
- * @return Resulting vector.
+ * @return @paramname{n} \f$ \cdot \f$ @paramname{v} Resulting vector.
  */
 vec mult(float n, vec v);
 
 /** Adding operation.
+ *
  * @param a Vector to operate.
  * @param b Vector to operate.
  * @return @paramname{a} + @paramname{b}.
@@ -196,43 +220,53 @@ vec mult(float n, vec v);
 vec add(vec a, vec b);
 
 /** Substracting operation.
+ *
  * @param a Vector to operate.
  * @param b Vector to operate.
  * @return @paramname{a} - @paramname{b}.
  */
 vec sub(vec a, vec b);
 
-/** Mean product.
+/** Inner product.
+ *
  * @param a Vector to operate.
  * @param b Vector to operate.
- * @return Scalar product value.
+ * @return @paramname{a} \f$ \cdot \f$ @paramname{b} scalar product value.
  */
 float dot(vec a, vec b);
 
 /** Compute the vector length.
+ *
  * @param v Input vector.
- * @return Vector length.
+ * @return \f$ \vert \f$ @paramname{v} \f$ \vert \f$ vector length.
  */
 float length(vec v);
 
-/** Compute a normalized vector copy (such that calling Aqua::length 1 will be
+/** Compute a normalized vector copy (such that calling length() 1 will be
  * returned).
+ *
  * @param v Vector to normalize.
- * @return Normalized copy.
+ * @return Normalized copy of the vector.
+ *
+ * @see length()
  */
 vec normalize(vec v);
 
 #ifdef HAVE_3D
-	/** Cross product.
-	 * @param a Vector to operate.
-	 * @param b Vector to operate.
-	 * @return Cross product value.
-	 */
-	vec cross(vec a, vec b);
+/** Cross product.
+ *
+ * @remarks Only available in the 3D version.
+ * @param a Vector to operate.
+ * @param b Vector to operate.
+ * @return @paramname{a} \f$ \times \f$ @paramname{b} crossed product vector.
+ */
+vec cross(vec a, vec b);
 #endif
 
-/** Get the number of digits of an integer decimal text representation
- * @param number Number that the number of digits are desired.
+/** Get the number of digits of an integer decimal text representation.
+ *
+ * @param number Number from which the number of digits should be computed.
+ * @return Number of digits.
  */
 unsigned int numberOfDigits(unsigned int number);
 
