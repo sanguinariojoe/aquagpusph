e7d9db0354f244fce0bbea247891c562d3575a62
Jose Luis Cercos-Pita <jl.cercos@upm.es>
2014-09-01 14:36:25 +0200

    Fixed tabulators and documentation

--- include/Fluid.h
+++ include/Fluid.h
@@ -16,6 +16,11 @@
  *  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/** @file
+ * @brief Fluid particles host allocated data.
+ * (See Aqua::InputOutput::Fluid for details)
+ */
+
 #ifndef FLUID_H_INCLUDED
 #define FLUID_H_INCLUDED
 
@@ -26,74 +31,89 @@
 namespace Aqua{ namespace InputOutput{
 
 /** @class Fluid Fluid.h Fluid.h
- * @brief Mirroring allocated in the host of the fluid data. This data storage
- * is used to initializae the fluid data and transfer it to the server, and to
- * download the data from the server in order to perform file outputs.
+ * @brief Fluid particles data allocated in the host.
+ * This class is a intermediate layer to download/uploaded the data between the
+ * host (see ::main) and the calculation server (see
+ * Aqua::CalcServer::CalcServer).
+ *
+ * More specifically, the particles data loaded from the input files will be
+ * stored here, and then transfered to the server.
+ *
+ * Later, during the simulation, the particles data will be downloaded from the
+ * server and stored here to be printed in the output files.
+ *
+ * @see Aqua::InputOutput::FileManager
+ * @see Aqua::InputOutput::Particles
  */
 class Fluid : public Aqua::Singleton<Aqua::InputOutput::Fluid>
 {
 public:
-	/** Constructor.
-	 */
-	Fluid();
+    /** Constructor.
+     */
+    Fluid();
 
-	/** Destructor.
-	 */
-	~Fluid();
+    /** Destructor.
+     */
+    ~Fluid();
 
-	/** Retrieves all data from server.
-	 * @return false if all gone right, true otherwise.
-	 */
-	bool retrieveData();
+    /** Retrieves all data from the calculation server.
+     *
+     * @return false if all gone right, true otherwise.
+     */
+    bool retrieveData();
 
-	/** Get the number of particles
-	 * @return Number of particles.
-	 */
-	unsigned int n(){return num_particles;}
-	/** Get the number of particles
-	 * @return Number of particles.
-	 */
-	unsigned int nParticles(){return n();}
+    /** Get the number of particles in this fluid.
+     *
+     * @return Number of particles.
+     */
+    unsigned int n(){return num_particles;}
 
-	/** Get the number of fluids
-	 * @return Number of fuid species.
-	 */
-	unsigned int nFluids(){return num_fluids;}
+    /** Get the number of particles in this fluid.
+     *
+     * @return Number of particles.
+     */
+    unsigned int nParticles(){return n();}
 
-	/** Fixed/sensor/regular part flag:
-	 *   - imove > 0 for regular fluid particles.
-	 *   - imove = 0 for sensors.
-	 *   - imove < 0 for boundary elements/particles.
-	 */
-	int *imove;
-	/// Fluid identifier.
-	int *ifluid;
-	/// Position \f$ \mathbf{r} \f$.
-	vec *pos;
-	/// Normal \f$ \mathbf{n} \f$.
-	vec *normal;
-	/// Velocity \f$ \mathbf{u} \f$.
-	vec *v;
-	/// Density \f$ \rho \f$.
-	float *dens;
-	/// Density variation rate \f$ \frac{\mathrm{d}\rho}{\mathrm{d}t} \f$.
-	float *drdt;
-	/// Acceleration \f$ \frac{\mathrm{d}\mathbf{u}}{\mathrm{d}t} \f$.
-	vec *f;
-	/// Mass \f$ m \f$.
-	float *mass;
-	/// Pressure \f$ p \f$.
-	float *press;
-	/** Shepard term \f$ \gamma(\mathbf{x}) = \int_{Omega}
+    /** Get the number of fluids.
+     *
+     * @return Number of fuid species.
+     */
+    unsigned int nFluids(){return num_fluids;}
+
+    /** Fixed/sensor/regular part flag:
+     *   - imove > 0 for regular fluid particles.
+     *   - imove = 0 for sensors.
+     *   - imove < 0 for boundary elements/particles.
+     */
+    int *imove;
+    /// Fluid identifier (Several Fluid instances may exist).
+    int *ifluid;
+    /// Position \f$ \mathbf{r} \f$.
+    vec *pos;
+    /// Normal \f$ \mathbf{n} \f$.
+    vec *normal;
+    /// Velocity \f$ \mathbf{u} \f$.
+    vec *v;
+    /// Density \f$ \rho \f$.
+    float *dens;
+    /// Density variation rate \f$ \frac{\mathrm{d}\rho}{\mathrm{d}t} \f$.
+    float *drdt;
+    /// Acceleration \f$ \frac{\mathrm{d}\mathbf{u}}{\mathrm{d}t} \f$.
+    vec *f;
+    /// Mass \f$ m \f$.
+    float *mass;
+    /// Pressure \f$ p \f$.
+    float *press;
+    /** Shepard term \f$ \gamma(\mathbf{x}) = \int_{\Omega}
            W(\mathbf{y} - \mathbf{x})
            \mathrm{d}\mathbf{x} \f$.
      */
-	float *shepard;
+    float *shepard;
 private:
-	/// Number of particles
-	unsigned int num_particles;
-	/// Number of fluids
-	unsigned int num_fluids;
+    /// Number of particles
+    unsigned int num_particles;
+    /// Number of fluids
+    unsigned int num_fluids;
 };
 
 }}  // namespace
--- src/Fluid.cpp
+++ src/Fluid.cpp
@@ -16,6 +16,11 @@
  *  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/** @file
+ * @brief Fluid particles host allocated data.
+ * (See Aqua::InputOutput::Fluid for details)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -30,97 +35,97 @@ namespace Aqua{ namespace InputOutput{
 
 Fluid::Fluid()
 {
-	unsigned int i, num_sensors;
-	char msg[512];
-	ProblemSetup *P = ProblemSetup::singleton();
-	ScreenManager *S = ScreenManager::singleton();
+    unsigned int i, num_sensors;
+    char msg[512];
+    ProblemSetup *P = ProblemSetup::singleton();
+    ScreenManager *S = ScreenManager::singleton();
 
-	num_particles = 0;
-	for(i=0;i<P->n_fluids;i++) {
-	    num_particles += P->fluids[i].n;
-	}
-	num_sensors = P->SensorsParameters.pos.size();
-	num_particles += num_sensors;
-	if(num_particles<=0){
-	    S->addMessageF(3, "Any particle found.\n");
-		exit(1);
-	}
-	sprintf(msg, "Number of particles = %u\n", num_particles);
-	S->addMessageF(1, msg);
+    num_particles = 0;
+    for(i=0;i<P->n_fluids;i++) {
+        num_particles += P->fluids[i].n;
+    }
+    num_sensors = P->SensorsParameters.pos.size();
+    num_particles += num_sensors;
+    if(num_particles<=0){
+        S->addMessageF(3, "Any particle found.\n");
+        exit(1);
+    }
+    sprintf(msg, "Number of particles = %u\n", num_particles);
+    S->addMessageF(1, msg);
 
-	num_fluids = P->n_fluids;
+    num_fluids = P->n_fluids;
 
-	imove = new int[num_particles];
-	ifluid = new int[num_particles];
-	pos = new vec[num_particles];
-	normal = new vec[num_particles];
-	v = new vec[num_particles];
-	dens = new float[num_particles];
-	drdt = new float[num_particles];
-	f = new vec[num_particles];
-	mass = new float[num_particles];
-	press = new float[num_particles];
-	shepard = new float[num_particles];
+    imove = new int[num_particles];
+    ifluid = new int[num_particles];
+    pos = new vec[num_particles];
+    normal = new vec[num_particles];
+    v = new vec[num_particles];
+    dens = new float[num_particles];
+    drdt = new float[num_particles];
+    f = new vec[num_particles];
+    mass = new float[num_particles];
+    press = new float[num_particles];
+    shepard = new float[num_particles];
 
-	for(i=num_particles-num_sensors;i<num_particles;i++) {
-	    imove[i]    = 0;
-	    ifluid[i]   = 0;
-	    pos[i]      = P->SensorsParameters.pos.at(i-num_particles+num_sensors);
-	    normal[i].x = 0.f; normal[i].y = 0.f;
-	    v[i].x      = 0.f; v[i].y = 0.f;
-	    dens[i]     = 0.f;
-	    drdt[i]     = 0.f;
-	    f[i].x      = 0.f; f[i].y = 0.f;
-	    mass[i]     = 0.f;
-	    press[i]    = 0.f;
-	    shepard[i]  = 0.f;
-	    #ifdef HAVE_3D
-	        normal[i].z = 0.f;
-	        normal[i].w = 0.f;
-	        v[i].z      = 0.f;
-	        v[i].w      = 0.f;
-	        f[i].z      = 0.f;
-	        f[i].w      = 0.f;
-	    #endif
-	}
+    for(i=num_particles-num_sensors;i<num_particles;i++) {
+        imove[i]    = 0;
+        ifluid[i]   = 0;
+        pos[i]      = P->SensorsParameters.pos.at(i-num_particles+num_sensors);
+        normal[i].x = 0.f; normal[i].y = 0.f;
+        v[i].x      = 0.f; v[i].y = 0.f;
+        dens[i]     = 0.f;
+        drdt[i]     = 0.f;
+        f[i].x      = 0.f; f[i].y = 0.f;
+        mass[i]     = 0.f;
+        press[i]    = 0.f;
+        shepard[i]  = 0.f;
+        #ifdef HAVE_3D
+            normal[i].z = 0.f;
+            normal[i].w = 0.f;
+            v[i].z      = 0.f;
+            v[i].w      = 0.f;
+            f[i].z      = 0.f;
+            f[i].w      = 0.f;
+        #endif
+    }
     sprintf(msg, "%u particles allocated OK, we can continue happily! ;-)\n", num_particles);
-	S->addMessageF(1, msg);
+    S->addMessageF(1, msg);
 }
 
 
 Fluid::~Fluid()
 {
-	delete[] imove; imove=0;
-	delete[] ifluid; ifluid=0;
-	delete[] pos; pos=0;
-	delete[] normal; normal=0;
-	delete[] v; v=0;
-	delete[] dens; dens=0;
-	delete[] drdt; drdt=0;
-	delete[] f; f=0;
-	delete[] mass; mass=0;
-	delete[] press; press=0;
-	delete[] shepard; shepard=0;
+    delete[] imove; imove=0;
+    delete[] ifluid; ifluid=0;
+    delete[] pos; pos=0;
+    delete[] normal; normal=0;
+    delete[] v; v=0;
+    delete[] dens; dens=0;
+    delete[] drdt; drdt=0;
+    delete[] f; f=0;
+    delete[] mass; mass=0;
+    delete[] press; press=0;
+    delete[] shepard; shepard=0;
 }
 
 bool Fluid::retrieveData()
 {
-	CalcServer::CalcServer *C = CalcServer::CalcServer::singleton();
-	int flag;
-	flag  = C->getData((void*)imove,   C->imove,   sizeof(cl_int)*num_particles);
-	flag |= C->getData((void*)ifluid,  C->ifluid,  sizeof(cl_int)*num_particles);
-	flag |= C->getData((void*)pos,     C->posin,   sizeof(vec)*num_particles);
-	flag |= C->getData((void*)normal,  C->normal,  sizeof(vec)*num_particles);
-	flag |= C->getData((void*)v,       C->vin,     sizeof(vec)*num_particles);
-	flag |= C->getData((void*)f,       C->fin,     sizeof(vec)*num_particles);
-	flag |= C->getData((void*)dens,    C->densin,  sizeof(cl_float)*num_particles);
-	flag |= C->getData((void*)drdt,    C->drdt,    sizeof(cl_float)*num_particles);
-	flag |= C->getData((void*)press,   C->press,   sizeof(cl_float)*num_particles);
-	flag |= C->getData((void*)mass,    C->mass,    sizeof(cl_float)*num_particles);
-	flag |= C->getData((void*)shepard, C->shepard, sizeof(cl_float)*num_particles);
-	if(flag)
-	    return true;
-	return false;
+    CalcServer::CalcServer *C = CalcServer::CalcServer::singleton();
+    int flag;
+    flag  = C->getData((void*)imove,   C->imove,   sizeof(cl_int)*num_particles);
+    flag |= C->getData((void*)ifluid,  C->ifluid,  sizeof(cl_int)*num_particles);
+    flag |= C->getData((void*)pos,     C->posin,   sizeof(vec)*num_particles);
+    flag |= C->getData((void*)normal,  C->normal,  sizeof(vec)*num_particles);
+    flag |= C->getData((void*)v,       C->vin,     sizeof(vec)*num_particles);
+    flag |= C->getData((void*)f,       C->fin,     sizeof(vec)*num_particles);
+    flag |= C->getData((void*)dens,    C->densin,  sizeof(cl_float)*num_particles);
+    flag |= C->getData((void*)drdt,    C->drdt,    sizeof(cl_float)*num_particles);
+    flag |= C->getData((void*)press,   C->press,   sizeof(cl_float)*num_particles);
+    flag |= C->getData((void*)mass,    C->mass,    sizeof(cl_float)*num_particles);
+    flag |= C->getData((void*)shepard, C->shepard, sizeof(cl_float)*num_particles);
+    if(flag)
+        return true;
+    return false;
 }
 
 }}  // namespace
