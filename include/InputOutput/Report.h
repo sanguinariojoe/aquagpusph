c7fe529caf56ade5de5c0b71ef9237bafcb77184
Jose Luis Cercos-Pita <jl.cercos@upm.es>
2014-09-03 13:14:27 +0200

    Documented the abstract class Report

--- include/InputOutput/Report.h
+++ include/InputOutput/Report.h
@@ -16,6 +16,11 @@
  *  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/** @file
+ * @brief Base class for all the report file managers.
+ * (See Aqua::InputOutput::Report for details)
+ */
+
 #ifndef REPORT_H_INCLUDED
 #define REPORT_H_INCLUDED
 
@@ -26,51 +31,65 @@ namespace Aqua{
 namespace InputOutput{
 
 /** \class Report Report.h InputOutput/Report.h
- *  Report file loader/saver base class.
+ * @brief Base class for all the report file managers.
+ *
+ * In AQUAgpusph the input/output managers are divided in 3 different types:
+ *   -# The simulation configuration files manager
+ *   -# The report file managers
+ *   -# The particles output file managers
+ *
+ * The reports are just output files that are relatively low hard disk
+ * demanding.
+ *
+ * This class allows the inherited ones to force the output file name (see
+ * file(const char* filename)) or to look the first file name which is not
+ * already existing in the system (see
+ * file(const char* basename, unsigned int startindex)).
+ *
+ * @see Aqua::InputOutput::State
+ * @see Aqua::InputOutput::Particles
  */
 class Report : public InputOutput
 {
 public:
-    /** Constructor
-     */
+    /// Constructor
     Report();
 
-    /** Destructor
-     */
+    /// Destructor
     virtual ~Report();
 
-    /** Save the data
+    /** @brief Save the data
      * @return false if all gone right, true otherwise.
      */
     virtual bool save(){return false;}
 
-    /** Load the data
+    /** @brief Load the data
      * @return false if all gone right, true otherwise.
      */
     virtual bool load(){return false;}
 
-    /** Get the used file.
+    /** @brief Get the used output file path.
      * @return The report file, NULL if it is not a file.
      */
     const char* file(){return (const char*)_output_file;}
 
 protected:
-    /** Set a new file.
-     * @param filename The new file to work. Optionally a null parameter can
-     * be passed in order to clear the stored file.
+    /** @brief Set the report file name.
+     * @param filename The file name. Optionally @paramname{filename} = null can
+     * be set in order to clear the stored file name.
      */
     void file(const char* filename);
 
-    /** Set the file name as the first ungenerate file.
-     * @param basename The base name of the file. In this base name the %d
-     * substring will be replaced by the first integer such that the file
-     * does not exist.
-     * @param startindex First index that will be tested.
+    /** @brief Look for the first non existing file name.
+     * @param basename The base name of the file. In this base name the `%d`
+     * string will be replaced by the first integer such that the file does not
+     * exist in the system.
+     * @param start_index First index that will be checked.
      * @return false if all gone right, true otherwise.
-     * @note If more than one %d substrings are found, just the first one will
-     * be replaced.
+     * @note If more than one `%d` strings are found in @paramname{basename},
+     * just the first one will be replaced.
      */
-    bool file(const char* basename, unsigned int startindex);
+    bool file(const char* basename, unsigned int start_index);
 
 private:
     /// Last file printed
