b96bb12e58aa3d8d3e69d763fb4b2c7c67d91a8b
Jose Luis Cercos-Pita <jl.cercos@upm.es>
2014-09-03 09:56:55 +0200

    Redocumented Singleton

--- include/Singleton.h
+++ include/Singleton.h
@@ -16,36 +16,58 @@
  *  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/** @file
+ * @brief Singleton instance.
+ * (See Aqua::Singleton for details)
+ */
+
 #ifndef SINGLETON_H_INCLUDED
 #define SINGLETON_H_INCLUDED
 
-// ----------------------------------------------------------------------------
-// Include standar header
-// ----------------------------------------------------------------------------
 #include <stdlib.h>
 
-/// @namespace Aqua Main AQUAgpusph namespace.
 namespace Aqua{
 
 /** \class Singleton Singleton.h Singleton.h
  * \brief Simple but versatile singleton instance (Meyers singleton).
+ *
+ * The singletons are instances that can be accessed everywhere even though it
+ * has not been passed as argument.
+ *
+ * To create a singleton instance just define the class as an inherited of this
+ * one:
+ *
+ * @code{.cpp}
+    #include <Singleton.h>
+    class MyClass : public Aqua::Singleton<MyClass>
+    {
+        ...
+    };
+   @endcode
+ *
+ * Then you can access your class everywhere you want (please, don't forget to
+ * include your class header):
+ *
+ * @code{.cpp}
+    MyClass *c = MyClass::singleton();
+   @endcode
+ *
+ * @warning This implementation will not support multithreading.
  */
 template<typename T> class Singleton
 {
 public:
-	/** Returns the instance of the class.
+	/** @brief Returns the instance of the class.
 	 * @return singleton instance.
 	 */
 	static T* singleton(){return _singletonPtr;}
 
 protected:
-	/** Constructor
-	 */
+	/// Constructor
 	Singleton(){
 	    _singletonPtr = (T*)this;
 	}
-	/** Destructor
-	 */
+	/// Destructor
 	~Singleton(){_singletonPtr = NULL;}
 
 private:
@@ -53,6 +75,7 @@ private:
 	static T *_singletonPtr;
 };
 
+/// Initialization of the singleton as a NULL pointer
 template <typename T> T* Singleton <T>::_singletonPtr = NULL;
 
 }   // namespaces
