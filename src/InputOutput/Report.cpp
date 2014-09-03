4dd12072a7bbb3bcf88f0f1b995f9dc13c6c4644
Jose Luis Cercos-Pita <jl.cercos@upm.es>
2014-09-03 13:16:25 +0200

    Added missed file documentation

--- src/InputOutput/Report.cpp
+++ src/InputOutput/Report.cpp
@@ -16,6 +16,11 @@
  *  along with AQUAgpusph.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/** @file
+ * @brief Base class for all the report file managers.
+ * (See Aqua::InputOutput::Report for details)
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
