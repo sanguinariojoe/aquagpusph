eebcea16050f7d1648a14c50a9e321a6620d1906
Jose Luis Cercos-Pita <jl.cercos@upm.es>
2015-03-11 10:05:38 +0100

    Removed the wave height sensor

--- examples/2D/souto_etal_2012_standingwave/cMake/run.sh
+++ examples/2D/souto_etal_2012_standingwave/cMake/run.sh
@@ -19,8 +19,6 @@ else
     echo ""
     echo "run.sh --run"
     echo "    Runs the simulation. You can stop simulation pressing the 'c' key"
-    echo "run.sh --plot h"
-    echo "    Plot the resulting wave height"
     echo "run.sh --plot t"
     echo "    Plot the performance"
     echo ""
