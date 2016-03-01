5fc61d7f400046b6fa17417fd121e2991f785114
Jose Luis Cercos Pita <jlcercos@gmail.com>
2016-03-01 21:27:51 +0100

    Added a lid-driven cavity example (SPHERIC Validation test case number 3)

---
+++ examples/2D/spheric_testcase3_liddriven/cMake/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $1 == "--run" ]]; then
+    rm -f Fluid.dat
+    rm -f BC.dat
+    rm -f AQUAgpusph.save.*.xml
+    rm -f log.*.html
+    rm -f *.vtu
+    rm -f *.pvd
+    rm -f Timing.dat
+    rm -f Performance.dat
+    @EXAMPLE_DEST_DIR@/Create.py
+    @BINARY_DIR@/AQUAgpusph2D -i Main.xml
+elif [[ $1 == "--plot" ]]; then
+    python @EXAMPLE_DEST_DIR@/plot_$2.py
+else
+    echo ""
+    echo "Usage: run.sh [--run/--plot]"
+    echo ""
+    echo "run.sh --plot t"
+    echo "    Plot the performance"
+    echo ""
+fi
