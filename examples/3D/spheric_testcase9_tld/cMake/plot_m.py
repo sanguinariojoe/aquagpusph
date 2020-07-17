ebb0fb13698802519156b2cdbdc245ca57bbfb8a
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2020-07-17 16:01:27 +0200

    Plotters for the SPHERIC validation test number 9 (3-D)

--- examples/3D/spheric_testcase9_tld/cMake/plot_m.py
+++ examples/3D/spheric_testcase9_tld/cMake/plot_m.py
@@ -32,131 +32,106 @@
 import sys
 import os
 from os import path
-try:
-    from PyQt4 import QtGui
-except:
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def readFile(filepath):
+    """ Read and extract data from a file
+    :param filepath File ot read
+    """
+    abspath = filepath
+    if not path.isabs(filepath):
+        abspath = path.join(path.dirname(path.abspath(__file__)), filepath)
+    # Read the file by lines
+    f = open(abspath, "r")
+    lines = f.readlines()
+    f.close()
+    data = []
+    for l in lines[:-1]:  # Skip the last line, which may be unready
+        l = l.strip()
+        fields = l.split('\t')
+        try:
+            data.append(map(float, fields))
+        except:
+            continue
+    # Transpose the data
+    return map(list, zip(*data))
+
+
+line = None
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+data = readFile('Ekin.dat')
+T = data[0][-1] / data[1][-1]
+exp_line, = ax.plot([0.0], [0.0],
+                    label=r'$\theta_{Exp}$',
+                    color="black",
+                    linestyle="--",
+                    linewidth=1.0)
+
+line, = ax.plot([0.0], [0.0],
+                label=r'$\theta_{SPH}$',
+                color="black",
+                linestyle="-",
+                linewidth=1.0)
+# Set some options
+ax.grid()
+ax.legend(loc='upper left')
+ax.set_xlim(0.0, 0.1)
+ax.set_ylim(-0.1, 0.1)
+ax.set_autoscale_on(False)
+ax.set_xlabel(r"$t \, [\mathrm{s}]$")
+ax.set_ylabel(r"$\theta \, [\mathrm{deg}]$")
+
+# Create a secondary set of axes for the moment
+ax2 = ax.twinx()
+mline, = ax2.plot([0.0], [0.0],
+                  label=r'$M_{SPH}$',
+                  color="blue",
+                  linewidth=1.0)
+# Set some options
+ax2.set_xlim(0, 0.1)
+ax2.set_ylim(-0.1, 0.1)
+ax2.set_autoscale_on(False)
+ax2.set_ylabel(r"$M_{fluid} \, [\mathrm{N} \cdot \mathrm{m}]$", color="blue")
+for tl in self.ax2.get_yticklabels():
+    tl.set_color("blue")
+
+
+# Animate
+def update(frame_index):
+    plt.tight_layout()
     try:
-        from PySide import QtGui
-    except:
-        raise ImportError("PyQt4 or PySide is required to use this tool")
-
-try:
-    from matplotlib.figure import Figure
-    from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-except:
-    raise ImportError("matplotlib is required to use this tool")
-
-
-class FigureController(FigureCanvas):
-    """Matplotlib figure widget controller"""
-
-    def __init__(self):
-        """Constructor"""
-        # Create the figure in the canvas
-        self.fig = Figure()
-        self.ax = self.fig.add_subplot(111)
-        FigureCanvas.__init__(self, self.fig)
-        # generates first "empty" plot
-        t = [0.0]
-        exp_a = [0.0]
-        a = [0.0]
-        m = [0.0]
-        self.exp_line, = self.ax.plot(t,
-                                      exp_a,
-                                      label=r'$\theta_{Exp}$',
-                                      color="black",
-                                      linestyle="--",
-                                      linewidth=1.0)
-        self.line, = self.ax.plot(t,
-                                  a,
-                                  label=r'$\theta_{SPH}$',
-                                  color="black",
-                                  linestyle="-",
-                                  linewidth=1.0)
-        # Set some options
-        self.ax.grid()
-        self.ax.legend(loc='best')
-        self.ax.set_xlim(0, 0.1)
-        self.ax.set_ylim(-0.1, 0.1)
-        self.ax.set_autoscale_on(False)
-        self.ax.set_xlabel(r"$t \, [\mathrm{s}]$", fontsize=21)
-        self.ax.set_ylabel(r"$\theta \, [\mathrm{deg}]$", fontsize=21)
-        # Create a second moment plot
-        self.ax2 = self.ax.twinx()
-        self.mline, = self.ax2.plot(t,
-                                    m,
-                                    label=r'$M_{SPH}$',
-                                    color="blue",
-                                    linewidth=1.0)
-        # Set some options
-        self.ax2.set_xlim(0, 0.1)
-        self.ax2.set_ylim(-0.1, 0.1)
-        self.ax2.set_autoscale_on(False)
-        self.ax2.set_ylabel(r"$M_{fluid} \, [\mathrm{N} \cdot \mathrm{m}]$",
-                            fontsize=21,
-                            color="blue")
-        for tl in self.ax2.get_yticklabels():
-            tl.set_color("blue")
-        # force the figure redraw
-        self.fig.canvas.draw()
-        # call the update method (to speed-up visualization)
-        self.timerEvent(None)
-        # start timer, trigger event every 1000 millisecs (=1sec)
-        self.timer = self.startTimer(1000)
-
-    def readFile(self, filepath):
-        """ Read and extract data from a file
-        :param filepath File ot read
-        """
-        abspath = filepath
-        if not path.isabs(filepath):
-            abspath = path.join(path.dirname(path.abspath(__file__)), filepath)
-        # Read the file by lines
-        f = open(abspath, "r")
-        lines = f.readlines()
-        f.close()
-        data = []
-        for l in lines[:-1]:  # Skip the last line, which may be unready
-            l = l.strip()
-            fields = l.split('\t')
-            try:
-                data.append(map(float, fields))
-            except:
-                continue
-        # Transpose the data
-        return map(list, zip(*data))
-
-    def timerEvent(self, evt):
-        """Custom timerEvent code, called at timer event receive"""
-        # Read and plot the new data
-        data = self.readFile('Motion.dat')
+        data = readFile('Motion.dat')
         t = data[0]
         exp_a = data[2]
         a = data[3]
         m = data[5]
-        self.exp_line.set_data(t, exp_a)
-        self.line.set_data(t, a)
-        self.mline.set_data(t, m)
+    except IndexError:
+        return
+    except FileNotFoundError:
+        return
+    exp_line.set_data(t, exp_a)
+    line.set_data(t, a)
+    mline.set_data(t, m)
 
-        self.ax.set_xlim(0, t[-1])
-        ymax_exp = max(abs(max(exp_a)), abs(min(exp_a)))
-        ymax_sph = max(abs(max(a)), abs(min(a)))
-        ymax = max((ymax_exp, ymax_sph))
-        self.ax.set_ylim(-1.1 * ymax, 1.1 * ymax)
+    ax.set_xlim(0, t[-1])
+    ymax_exp = max(abs(max(exp_a)), abs(min(exp_a)))
+    ymax_sph = max(abs(max(a)), abs(min(a)))
+    ymax = max((ymax_exp, ymax_sph))
+    ax.set_ylim(-1.1 * ymax, 1.1 * ymax)
 
-        self.ax2.set_xlim(0, t[-1])
-        ymax = max(abs(max(m)), abs(min(m)))
-        self.ax2.set_ylim(-1.1 * ymax, 1.1 * ymax)
-        for tl in self.ax2.get_yticklabels():
-            tl.set_color("blue")
-
-        # Redraw
-        self.fig.canvas.draw()
+    ax2.set_xlim(0, t[-1])
+    ymax = max(abs(max(m)), abs(min(m)))
+    ax2.set_ylim(-1.1 * ymax, 1.1 * ymax)
+    for tl in ax2.get_yticklabels():
+        tl.set_color("blue")
 
 
-if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
-    widget = FigureController()
-    widget.setWindowTitle("Roll angle")
-    widget.show()
-    sys.exit(app.exec_())
+update(0)
+ani = animation.FuncAnimation(fig, update, interval=1000)
+plt.show()
