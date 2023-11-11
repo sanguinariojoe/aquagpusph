ef045f3c169dc96c0ddd19eded42660b5845d504
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2023-11-11 11:14:12 +0100

    Python tests

---
+++ tests/2D/Python/cMake/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+# Clean up before running, so previous failures are not contaminating the
+# results
+rm -f AQUAgpusph.save.* log.* set0.*.dat vars.out
+
+# Run the "simulation"
+@BINARY_DIR@/AQUAgpusph2D -i main.xml
