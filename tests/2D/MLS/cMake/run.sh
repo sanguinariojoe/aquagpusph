9534fed74af03ed37127cc7c40346735c8360a46
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2023-11-23 15:31:41 +0100

    More robust result checker

--- tests/2D/MLS/cMake/run.sh
+++ tests/2D/MLS/cMake/run.sh
@@ -2,16 +2,12 @@
 
 set -e
 
+# Clean up before running, so previous failures are not contaminating the
+# results
+rm -f AQUAgpusph.save.* log.* set0.*.dat set1.*.dat rmse.dat
+
 # Run the "simulation", and clear everything but the main result
 @BINARY_DIR@/AQUAgpusph2D -i main.xml
-rm -f AQUAgpusph.save.* fluid.* log.*
 
-# Collect the result
-rmse=`sed -n '2{p;q;}' rmse.dat`
-
-# We have 500 particles, from which 3 ones have an error of 1, while the other
-# ones have errors of ~0. Therefore rmse should be ~sqrt(3 / 500) = 0.077
-# Let's get as an acceptable value a 10% bigger error, i.e. 0.085
-if (( rmse > 0.085 )); then
-    exit 1
-fi
+# Check the result
+python @TEST_DEST_DIR@/check.py
--- tests/3D/MLS/cMake/run.sh
+++ tests/3D/MLS/cMake/run.sh
@@ -6,12 +6,5 @@ set -e
 @BINARY_DIR@/AQUAgpusph -i main.xml
 rm -f AQUAgpusph.save.* fluid.* log.*
 
-# Collect the result
-rmse=`sed -n '2{p;q;}' rmse.dat`
-
-# We have 500 particles, from which 3 ones have an error of 1, while the other
-# ones have errors of ~0. Therefore rmse should be ~sqrt(3 / 500) = 0.077
-# Let's get as an acceptable value a 10% bigger error, i.e. 0.085
-if (( rmse > 0.085 )); then
-    exit 1
-fi
+# Check the result
+python @TEST_DEST_DIR@/check.py
